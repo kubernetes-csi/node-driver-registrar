25e03f913d6b1643453754062b831e8c59614aac
Michelle Au <msau@google.com>
2018-11-14 11:45:18 -0800

    Update csi/v0 import to csi/

--- pkg/connection/connection_test.go
+++ pkg/connection/connection_test.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/container-storage-interface/spec/lib/go/csi/v0"
+	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/golang/mock/gomock"
 	"github.com/kubernetes-csi/csi-test/driver"
 )
